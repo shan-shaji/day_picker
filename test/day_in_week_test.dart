7b731cc1fe58a9a1ce7d19793bef29c2d77373a2
shan-shaji <abrahamshanshaji@gmail.com>
2021-09-10 19:20:32 +0530

    test: add `DayInWeek` test

---
+++ test/day_in_week_test.dart
@@ -0,0 +1,21 @@
+import 'dart:math';
+
+import 'package:day_picker/model/day_in_week.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('By default isSelected will be false', () {
+    DayInWeek s = DayInWeek('Tuesday');
+
+    expect(s.isSelected, false);
+    expect(s.dayName, 'Tuesday');
+  });
+
+  test('when called toggleSelected `false` will become `true`', () {
+    DayInWeek s = DayInWeek('Tuesday');
+
+    expect(s.isSelected, false);
+    s.toggleIsSelected();
+    expect(s.isSelected, true);
+  });
+}
